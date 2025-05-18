fc20b3239e2b7ef15f8b1546ab01d49b115df5ae
BaomaDev <jessesimanjuntaks@gmail.com>
2025-05-18 14:42:17 +0700

    feat: followed teh tutorial of auth & smart notif

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import '../screens/home.dart';
+import '../screens/login.dart';
+import '../screens/register.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth_trial/screens/second_screen.dart';
+import 'package:firebase_auth_trial/services/notification_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NotificationService.initializeNotification();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: 'login',
+      routes: {
+        'home': (context) => const HomeScreen(),
+        'login': (context) => const LoginScreen(),
+        'register': (context) => const RegisterScreen(),
+        'second': (context) => const SecondScreen(),
+      },
+      navigatorKey: navigatorKey,
+      title: 'Firebase Auth & smart notif',
+    );
+  }
+}
